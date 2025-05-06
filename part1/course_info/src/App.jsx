72502c8d2de82146f2329345f2f0101a4dd78cc6
PetteriMoilanen <115905059+PetteriMoilanen@users.noreply.github.com>
2025-05-06 22:46:03 +0000

    ex 1.1-2

--- part1/course_info/src/App.jsx
+++ part1/course_info/src/App.jsx
@@ -7,19 +7,43 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+  const Header = (props) => {
+    return (
+      <div>
+        <h1>{props.course}</h1>
+      </div>
+    )
+  }
+  const Part = (props) => {
+    return (
+      <div>
+        <p>{props.part} {props.exercises} </p>
+      </div>
+    )  
+  }
+  const Content = () => {
+    return (
+      <div>
+        <Part part={part1} exercises={exercises1} />
+        <Part part={part2} exercises={exercises3} />
+        <Part part={part3} exercises={exercises3} />
+      </div>
+    )
+  }
+
+  const Total = (props) => {
+    return (
+      <div>
+        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      </div>
+    )
+  }
+
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
